b8721a5898a30e7e2279ed75478d8397d83f2b58
karki-19 <deepakbisht807@gmail.com>
2023-12-13 13:59:05 +0530

    add register controller

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -43,7 +43,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, "Password is Required"],
     },
-    refreshToken: {
+    refreshtoken: {
       type: String,
     },
   },
@@ -54,7 +54,7 @@ const userSchema = new mongoose.Schema(
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = bcrypt.hash(this.password, 10);
+  this.password =await bcrypt.hash(this.password, 10);
   next();
 });
 userSchema.methods.isPasswordCorrect = async function (password) {
@@ -87,3 +87,5 @@ userSchema.methods.refreshToken = function () {
 };
 
 export const User = mongoose.model("User", userSchema);
+
+
